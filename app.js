8a3306b1db9c5536953112b45f0cd6718971daeb
Crithane <shane_121_@hotmail.com>
2017-07-17 18:49:40 +0100

    Remove unnecessary line by line commenting

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ var domain
 var knwlInstance = new Knwl('english')
 var siteDir
 var words
-// var phoneNumberRegex = /^(((\+44)? ?(\(0\))? ?)|(0))( ?[0-9]{3,4}){3}$/
-// var postCodeRegex = /^['|"]{0,1}[A-Za-z]{1,2}[0-9]{1,2}[ \n]{0,1}[\n ]{0,1}?[0-9][A-Za-z]{2}['|"]{0,1}$/
 var bodyG //Global body variable
 var finalEmails = [] //Final processed emails
 var finalPhoneNumbers = [] //Final processed phonenumbers
@@ -49,54 +47,53 @@ else {
  *     - Uses links/body to find email addresses and save to array
  * 4. Outputs the scraped data to console and json file in corresponding domain folder
  *
- *
  * @type {Promise}
  */
 var body = makeRequest() //sets the first promise in the sequence
-body.then(function (body) { // executes first promise returning body (HTML body)
-    words = getWords(body) // Uses getWords() to grab all the words from the body
-    bodyG = body // Saves the body as a global variable
-    return loadToCheerio(body) //Loads the next promise
-}).then(function ($) { // Uses the loadToCheerio() function to load the body into the cheerio api
-    homePageTitle = $('title').text() //Save HTML title to variable
-    return findHyperLinks($) //Loads the next promise
-}).then(function (links) { //Executes findHyperLinks() returning all HREF's as links[]
-    findPhoneNumber(links) //sends links[] to findphonenumber
-    findEmails(bodyG) //sends global body variable to the findEmails() function
-    hyperLinks = links //Saves links to global variable
+body.then(function (body) {
+    words = getWords(body)
+    bodyG = body
+    return loadToCheerio(body)
+}).then(function ($) {
+    homePageTitle = $('title').text()
+    return findHyperLinks($)
+}).then(function (links) {
+    findPhoneNumber(links)
+    findEmails(bodyG)
+    hyperLinks = links
 }).then(function () {
-    //Executes once emails and phone numbers have been stored
     scrapeObj = {
-        'website': domain, //Saves scraped data to an object
+        'website': domain,
         'URL': 'http://www.' + domain,
         'homepageName': homePageTitle,
         'emails': finalEmails,
         'telephone': finalPhoneNumbers,
     }
-    var json = JSON.stringify(scrapeObj, null, 4) //Convert object to string
+    var json = JSON.stringify(scrapeObj, null, 4)
     var domainS = domain.split('.')[0] // Take away the domain suffix, so domain name can be used as filename
-    fs.writeFile(siteDir + domainS + '.json', json, 'utf8') // Write json to file domain.json
+    fs.writeFile(siteDir + domainS + '.json', json, 'utf8')
     console.dir(scrapeObj, { // Display json in console
         depth: null,
         colors: true,
     })
-}).catch(function (error) { //Catch any errors produced by promises
-    console.log(error) //Log any errors caught
+}).catch(function (error) {
+    console.log(error)
 })
 
 /**
  * Grabs the domain part of any given email by splitting it at the "@" symbol
+ * Also creates a directory for the given domain if one does not already exist
  *
  * @param ie - Email entered in arguments
  * @returns {*} - Domain
  */
 function grabDomain (ie) {
-    domain = ie.split('@')[1] //Splits the email at the @ symbol taking the second part of the split as the domain.
-    siteDir = './scraped sites/' + domain + '/' //Set directory for individual domains
-    if (!fs.existsSync(siteDir)) { //Check if directory already exists
-        fs.mkdirSync(siteDir) //Make the directory
+    domain = ie.split('@')[1]
+    siteDir = './scraped sites/' + domain + '/'
+    if (!fs.existsSync(siteDir)) {
+        fs.mkdirSync(siteDir)
     }
-    return domain //Returns the grabbed domain.
+    return domain
 }
 
 /**
@@ -108,23 +105,23 @@ function grabDomain (ie) {
  */
 function makeRequest () {
     return new Promise(function (resolve, reject) {
-        request({ //Make request to the website
+        request({
             'rejectUnauthorized': false,
-            'url': 'http://www.' + grabDomain(inputEmail), //Use grabDomain() to get the domain from email
-            'method': 'GET' //Use GET method to retrieve site data
+            'url': 'http://www.' + grabDomain(inputEmail),
+            'method': 'GET'
         }, function (error, response, body) {
             if (!error) {
-                if (response.statusCode === 200) { //Status code 200 signifies success
+                if (response.statusCode === 200) {
                     console.log('StatusCode OK')
-                    resolve(body) //Resolves the promise, sending the data back
+                    resolve(body)
                 }
                 else {
-                    console.log('Bad response code: ' + response.statusCode) //Returns the bad response code
-                    reject(response.statusCode) //Rejects the promise, sends back the error code
+                    console.log('Bad response code: ' + response.statusCode)
+                    reject(response.statusCode)
                 }
             }
             else {
-                console.log(error) //Displays any error messages returned by the request
+                console.log(error)
                 console.log('Unable to scrape given domain, exiting program (see error above)')
                 process.exit()
             }
@@ -141,13 +138,13 @@ function makeRequest () {
  */
 function loadToCheerio (body) {
     return new Promise(function (resolve, reject) {
-        var $ = cheerio.load(body, { //Using cheerio to store the pages HTML in a variable
+        var $ = cheerio.load(body, {
             normalizeWhitespace: true,
             xmlMode: true,
             decodeEntities: true,
             withDomLvl1: true,
         })
-        resolve($) //Returns cheerio variable to promise chain
+        resolve($)
     })
 }
 
@@ -158,7 +155,7 @@ function loadToCheerio (body) {
  * @param array - Array to check for duplicate in
  * @returns {boolean} - Returns true if duplicate found
  */
-function checkDuplicate (entry, array) { //Checks if a value already exists within an array
+function checkDuplicate (entry, array) {
     for (var i = 0; i < array.length; i++) {
         if (array[i] == entry) {
             return true
@@ -178,26 +175,25 @@ function checkDuplicate (entry, array) { //Checks if a value already exists with
  */
 function findPhoneNumber (links) {
     // console.log(links);
-    links.forEach(function (link) { //A method to find phone numbers on websites using tel: hyperlinks
+    links.forEach(function (link) { // Method 1
         if (link.indexOf('tel:') !== -1) {
-            link = link.split(':')[1] //Splits string after : to get the phone number
-            link = link.replace(/\s/g, '') //Removes spaces in phone number
-            if (link.charAt(0) == 0) { //Replaces first 0 with country code
-                link = link.replace(link.charAt(0), '+44') // UK country code +44
+            link = link.split(':')[1]
+            link = link.replace(/\s/g, '')
+            if (link.charAt(0) == 0) {
+                link = link.replace(link.charAt(0), '+44')
             }
-            if (link.length > 5) { //Only pushes to array if phoneumber more than 5 digits
-                // console.log("FINAL PHONENUMBER: " + link);
+            if (link.length > 5) {
                 if (!checkDuplicate(link, finalPhoneNumbers)) {
                     finalPhoneNumbers.push(link)
                 }
             }
         }
     })
-    var phones = knwlInstance.get('internationalPhones') //Uses Knwl to get phone numbers not listed as hyperlinks
+    var phones = knwlInstance.get('internationalPhones') // Method 2
     phones.forEach(function (phone) {
         if (phone) {
-            if (!checkDuplicate(phone['number'], finalPhoneNumbers)) { //Checks if each number is a duplicate
-                finalPhoneNumbers.push(phone['number']) //Pushes to finalPhoneNumbers array
+            if (!checkDuplicate(phone['number'], finalPhoneNumbers)) {
+                finalPhoneNumbers.push(phone['number'])
             }
         }
     })
@@ -206,23 +202,23 @@ function findPhoneNumber (links) {
 /**
  * Grabs all emails on the website using Knwl and pushes them to the finalEmails array
  *
- * @returns {boolean} - True if emails are found
+ * @returns {boolean} - True if emails are found, false if not
  */
-function findEmails () { //Uses Knwl to retrieve emails from the page
+function findEmails () {
     var emails = knwlInstance.get('emails')
-    var anyEmails = false //Variable to declare if Knwl was able to find any emails or not
+    var anyEmails = false
     emails.forEach(function (email) {
         if (email) {
-            if (!checkDuplicate(email['address'], finalEmails)) { //Checks array for duplicate entries
-                finalEmails.push(email['address']) //If no duplicates, pushes email to array
+            if (!checkDuplicate(email['address'], finalEmails)) {
+                finalEmails.push(email['address'])
             }
-            anyEmails = true // Sets emails found variable to true
+            anyEmails = true
         }
     })
     if (anyEmails)
-        return true // Returns true if emails were found
+        return true
     else
-        return false // Returns false if no emails were found
+        return false
 }
 
 /**
@@ -230,16 +226,16 @@ function findEmails () { //Uses Knwl to retrieve emails from the page
  *
  * @param $ - Cheerio API loaded HTML body
  * @returns {Promise}
- * @resolve {array} - Resolves and retruns array of hyperlinks
+ * @resolve {array} - Resolves and returns array of hyperlinks
  */
-function findHyperLinks ($) { //function that grabs all hyperlinks in HREF tags
+function findHyperLinks ($) {
     return new Promise(function (resolve, reject) {
-        anchors = $('a') //grabs the anchors from cheerio
+        anchors = $('a') //Grabs the anchors from cheerio
         links = []
-        $(anchors).each(function (i, anchor) { //For each hyperlink on the website grab the href.
-            links.push($(anchor).attr('href')) //Send href to findPhoneNumber()
+        $(anchors).each(function (i, anchor) { //For each anchor on the website grab the href.
+            links.push($(anchor).attr('href'))
         })
-        resolve(links) //Returns links found to the promise chain
+        resolve(links)
     })
 }
 
@@ -248,7 +244,7 @@ function findHyperLinks ($) { //function that grabs all hyperlinks in HREF tags
  *
  * @param body - The HTML body returned from the original request
  */
-function getWords (body) { //Uses Knwl to grab all words on the website
+function getWords (body) {
     knwlInstance.init(body)
     return (knwlInstance.words.get('linkWordsCasesensitive'))
 }
